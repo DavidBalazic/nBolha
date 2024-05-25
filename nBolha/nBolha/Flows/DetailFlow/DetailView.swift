692a70bd5a36f12a494332085ddc201dc1496993
davidB <d.balazic@nchain.com>
2024-05-25 21:54:37 +0200

    feat: separating view into subviews

--- nBolha/nBolha/Flows/DetailFlow/DetailView.swift
+++ nBolha/nBolha/Flows/DetailFlow/DetailView.swift
@@ -23,87 +23,26 @@ public struct DetailView: View {
     public var body: some View {
         ZStack {
             ScrollView(showsIndicators: false) {
-                CarouselView(
-                    showLikeButton: true,
-                    advertisement: viewModel.advertisement,
-                    likeButtonTapped: {
-                        viewModel.likeAdvertisementTapped(advertisementId: viewModel.advertisement?.advertisementId ?? 0)
-                    },
-                    dislikeButtonTapped: {
-                        viewModel.dislikeAdvertisementTapped(advertisementId: viewModel.advertisement?.advertisementId ?? 0)
-                    },
-                    isDialogPresented: $isDialogPresented
-                )
-                .padding(.bottom, NCConstants.Margins.extraLarge.rawValue)
                 VStack(spacing: NCConstants.Margins.extraLarge.rawValue) {
-                    VStack(spacing: NCConstants.Margins.large.rawValue) {
-                        Text(viewModel.advertisement?.title ?? "No title")
-                            .textStyle(.subtitle01)
-                            .foregroundStyle(Color(UIColor.text01!))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        HStack {
-                            Text(PriceFormatter.formatPrice(viewModel.advertisement?.price ?? 0))
-                                .textStyle(.subtitle01)
-                                .foregroundStyle(Color(UIColor.text01!))
-                            Spacer()
-                            SwifUIChipView(
-                                title: viewModel.advertisement?.address ?? "No address",
-                                size: .medium,
-                                style: .init(disabledBackgroundColor: Color(.background05!), disabledBorderColor: .clear, disabledTintColor: Color(.brandSecondary!)),
-                                state: .disabled,
-                                leadingIcon: Image(uiImage: .mapPin)
-                            )
-                        }
+                    CarouselView(
+                        showLikeButton: true,
+                        advertisement: viewModel.advertisement,
+                        likeButtonTapped: {
+                            viewModel.likeAdvertisementTapped(advertisementId: viewModel.advertisement?.advertisementId ?? 0)
+                        },
+                        dislikeButtonTapped: {
+                            viewModel.dislikeAdvertisementTapped(advertisementId: viewModel.advertisement?.advertisementId ?? 0)
+                        },
+                        isDialogPresented: $isDialogPresented
+                    )
+                    VStack(spacing: NCConstants.Margins.extraLarge.rawValue) {
+                        advertisementDetails()
+                        descriptionSection()
+                        conditionSection()
                     }
-                    VStack(alignment: .leading, spacing: NCConstants.Margins.small.rawValue) {
-                        Text(viewModel.advertisement?.description ?? "No description")
-                            .textStyle(.body02)
-                            .foregroundStyle(Color(UIColor.text01!))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .lineLimit(viewModel.shouldShowTextLimit ? 4 : nil)
-                        
-                        if viewModel.showButton {
-                            Button(action: {
-                                viewModel.toggleTextLimit()
-                            }) {
-                                HStack {
-                                    Text(viewModel.shouldShowTextLimit ? "View more" : "View less")
-                                        .textStyle(.subtitle03)
-                                        .foregroundStyle(Color(UIColor.brandSecondary!))
-                                    Image(uiImage: viewModel.shouldShowTextLimit ? .chevronDown : .chevronUp)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 18, height: 18)
-                                        .foregroundColor(Color(UIColor.brandSecondary!))
-                                    
-                                }
-                            }
-                        }
-                    }
-                    VStack(spacing: NCConstants.Margins.huge.rawValue) {
-                        HStack {
-                            Text("Condition: ")
-                                .textStyle(.subtitle02)
-                                .foregroundStyle(Color(UIColor.text01!))
-                            Text(viewModel.advertisement?.condition ?? "No condition")
-                                .textStyle(.body02)
-                                .foregroundStyle(Color(UIColor.text01!))
-                            Spacer()
-                        }
-                        SwiftUIButton(
-                            text: "Contact seller",
-                            leadingIcon:  SwiftUIButton.ButtonIcon.init(
-                                image: Image(uiImage: .envelope),
-                                size: .large
-                            ),
-                            tapped: {
-                                viewModel.contactSellerTapped()
-                            }
-                        )
-                        .fixedSize()
-                    }
+                    .padding(.horizontal, NCConstants.Margins.large.rawValue)
                 }
-                .padding(.horizontal, NCConstants.Margins.large.rawValue)
+                
             }
             if isDialogPresented {
                 CarouselViewDialog(
@@ -117,6 +56,83 @@ public struct DetailView: View {
                     viewModel.enableNavigations()
                 }
             }
+        }
+    }
+    
+    @ViewBuilder
+    private func advertisementDetails() -> some View {
+        VStack(spacing: NCConstants.Margins.large.rawValue) {
+            Text(viewModel.advertisement?.title ?? "No title")
+                .textStyle(.subtitle01)
+                .foregroundStyle(Color(UIColor.text01!))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            HStack {
+                Text(PriceFormatter.formatPrice(viewModel.advertisement?.price ?? 0))
+                    .textStyle(.subtitle01)
+                    .foregroundStyle(Color(UIColor.text01!))
+                Spacer()
+                SwifUIChipView(
+                    title: viewModel.advertisement?.address ?? "No address",
+                    size: .medium,
+                    style: .init(disabledBackgroundColor: Color(.background05!), disabledBorderColor: .clear, disabledTintColor: Color(.brandSecondary!)),
+                    state: .disabled,
+                    leadingIcon: Image(uiImage: .mapPin)
+                )
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func descriptionSection() -> some View {
+        VStack(alignment: .leading, spacing: NCConstants.Margins.small.rawValue) {
+            Text(viewModel.advertisement?.description ?? "No description")
+                .textStyle(.body02)
+                .foregroundStyle(Color(UIColor.text01!))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .lineLimit(viewModel.shouldShowTextLimit ? 4 : nil)
+            
+            if viewModel.showButton {
+                Button(action: {
+                    viewModel.toggleTextLimit()
+                }) {
+                    HStack {
+                        Text(viewModel.shouldShowTextLimit ? "View more" : "View less")
+                            .textStyle(.subtitle03)
+                            .foregroundStyle(Color(UIColor.brandSecondary!))
+                        Image(uiImage: viewModel.shouldShowTextLimit ? .chevronDown : .chevronUp)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18, height: 18)
+                            .foregroundColor(Color(UIColor.brandSecondary!))
+                    }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func conditionSection() -> some View {
+        VStack(spacing: NCConstants.Margins.huge.rawValue) {
+            HStack {
+                Text("Condition: ")
+                    .textStyle(.subtitle02)
+                    .foregroundStyle(Color(UIColor.text01!))
+                Text(viewModel.advertisement?.condition ?? "No condition")
+                    .textStyle(.body02)
+                    .foregroundStyle(Color(UIColor.text01!))
+                Spacer()
+            }
+            SwiftUIButton(
+                text: "Contact seller",
+                leadingIcon:  SwiftUIButton.ButtonIcon.init(
+                    image: Image(uiImage: .envelope),
+                    size: .large
+                ),
+                tapped: {
+                    viewModel.contactSellerTapped()
+                }
+            )
+            .fixedSize()
         }
     }
 }
--- nBolha/nBolha/Flows/FilterFlow/FilterView.swift
+++ nBolha/nBolha/Flows/FilterFlow/FilterView.swift
@@ -25,6 +25,25 @@ struct FilterView: View {
     }
 
     var body: some View {
+       filterHeader()
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: NCConstants.Margins.extraLarge.rawValue) {
+               radioButtonSection()
+               checkBoxSection()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, NCConstants.Margins.extraLarge.rawValue)
+            .padding(.bottom, NCConstants.Margins.extraLarge.rawValue)
+        }
+        SwiftUIButton(text: "View results") {
+            applyFilters(viewModel.selectedCheckBoxes, viewModel.selectedRadioButton)
+            isFilterTapped.toggle()
+        }
+        .padding(.horizontal, 20)
+    }
+    
+    @ViewBuilder
+    private func filterHeader() -> some View {
         HStack {
             Button(action: {
                 viewModel.resetFilters()
@@ -47,111 +66,107 @@ struct FilterView: View {
             .frame(maxWidth: .infinity, alignment: .trailing)
         }
         .padding(NCConstants.Margins.extraLarge.rawValue)
-        ScrollView(showsIndicators: false) {
-            VStack(spacing: NCConstants.Margins.extraLarge.rawValue) {
-                VStack(alignment: .leading, spacing: NCConstants.Margins.medium.rawValue) {
-                    HStack {
-                        Image(.sortByIcon)
-                        Text("Sort by")
-                            .textStyle(.subtitle02)
-                            .foregroundStyle(Color(UIColor.brandPrimary!))
-                        Spacer()
-                    }
-                    RadioButton(tag: .newest, selection: $viewModel.selectedRadioButton, label: "Newest to oldest")
-                    RadioButton(tag: .oldest, selection: $viewModel.selectedRadioButton, label: "Oldest to newest")
-                    RadioButton(tag: .lowToHigh, selection: $viewModel.selectedRadioButton, label: "Price: low to high")
-                    RadioButton(tag: .highToLow, selection: $viewModel.selectedRadioButton, label: "Price: high to low")
-                }
-                VStack(alignment: .leading, spacing: NCConstants.Margins.medium.rawValue) {
-                    HStack {
-                        Image(.conditionIcon)
-                        Text("Condition")
-                            .textStyle(.subtitle02)
-                            .foregroundStyle(Color(UIColor.brandPrimary!))
-                        Spacer()
-                    }
-                    SwiftUICheckBox(
-                        title: "New with tags",
-                        text: "A brand-new, unused item with tags or original packaging.",
-                        checked: Binding(
-                            get: {
-                                viewModel.selectedCheckBoxes.contains(.withTags)
-                            },
-                            set: { isChecked in
-                                viewModel.toggleCondition(.withTags)
-                            }
-                        ),
-                        style: SwiftUICheckBox.Style(
-                            font: .subtitle02,
-                            textColor: Color(.text02!),
-                            iconColor: Color(.text02!),
-                            disabledIconColor: Color(.brandSecondary!)
-                        )
-                    )
-                    SwiftUICheckBox(
-                        title: "New without tags",
-                        text: "A brand-new, unused item without tags or original packaging.",
-                        checked: Binding(
-                            get: {
-                                viewModel.selectedCheckBoxes.contains(.withoutTags)
-                            },
-                            set: { isChecked in
-                                viewModel.toggleCondition(.withoutTags)
-                            }
-                        ),
-                        style: SwiftUICheckBox.Style(
-                            font: .subtitle02,
-                            textColor: Color(.text02!),
-                            iconColor: Color(.text02!),
-                            disabledIconColor: Color(.brandSecondary!)
-                        )
-                    )
-                    SwiftUICheckBox(
-                        title: "Very good",
-                        text: "A lightly used item that may have slight imperfections.",
-                        checked: Binding(
-                            get: {
-                                viewModel.selectedCheckBoxes.contains(.veryGood)
-                            },
-                            set: { isChecked in
-                                viewModel.toggleCondition(.veryGood)
-                            }
-                        ),
-                        style: SwiftUICheckBox.Style(
-                            font: .subtitle02,
-                            textColor: Color(.text02!),
-                            iconColor: Color(.text02!),
-                            disabledIconColor: Color(.brandSecondary!)
-                        )
-                    )
-                    SwiftUICheckBox(
-                        title: "Satisfactory",
-                        text: "A frequently used item with imperfections and signs of wear.",
-                        checked: Binding(
-                            get: {
-                                viewModel.selectedCheckBoxes.contains(.satisfactory)
-                            },
-                            set: { isChecked in
-                                viewModel.toggleCondition(.satisfactory)
-                            }
-                        ),
-                        style: SwiftUICheckBox.Style(
-                            font: .subtitle02,
-                            textColor: Color(.text02!),
-                            iconColor: Color(.text02!),
-                            disabledIconColor: Color(.brandSecondary!)
-                        )
-                    )
-                }
+    }
+    
+    @ViewBuilder
+    private func radioButtonSection() -> some View {
+        VStack(alignment: .leading, spacing: NCConstants.Margins.medium.rawValue) {
+            HStack {
+                Image(.sortByIcon)
+                Text("Sort by")
+                    .textStyle(.subtitle02)
+                    .foregroundStyle(Color(UIColor.brandPrimary!))
+                Spacer()
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, NCConstants.Margins.extraLarge.rawValue)
-            .padding(.bottom, NCConstants.Margins.extraLarge.rawValue)
+            RadioButton(tag: .newest, selection: $viewModel.selectedRadioButton, label: "Newest to oldest")
+            RadioButton(tag: .oldest, selection: $viewModel.selectedRadioButton, label: "Oldest to newest")
+            RadioButton(tag: .lowToHigh, selection: $viewModel.selectedRadioButton, label: "Price: low to high")
+            RadioButton(tag: .highToLow, selection: $viewModel.selectedRadioButton, label: "Price: high to low")
         }
-        SwiftUIButton(text: "View results") {
-            applyFilters(viewModel.selectedCheckBoxes, viewModel.selectedRadioButton)
-            isFilterTapped.toggle()
+    }
+    
+    @ViewBuilder
+    private func checkBoxSection() -> some View {
+        VStack(alignment: .leading, spacing: NCConstants.Margins.medium.rawValue) {
+            HStack {
+                Image(.conditionIcon)
+                Text("Condition")
+                    .textStyle(.subtitle02)
+                    .foregroundStyle(Color(UIColor.brandPrimary!))
+                Spacer()
+            }
+            SwiftUICheckBox(
+                title: "New with tags",
+                text: "A brand-new, unused item with tags or original packaging.",
+                checked: Binding(
+                    get: {
+                        viewModel.selectedCheckBoxes.contains(.withTags)
+                    },
+                    set: { isChecked in
+                        viewModel.toggleCondition(.withTags)
+                    }
+                ),
+                style: SwiftUICheckBox.Style(
+                    font: .subtitle02,
+                    textColor: Color(.text02!),
+                    iconColor: Color(.text02!),
+                    disabledIconColor: Color(.brandSecondary!)
+                )
+            )
+            SwiftUICheckBox(
+                title: "New without tags",
+                text: "A brand-new, unused item without tags or original packaging.",
+                checked: Binding(
+                    get: {
+                        viewModel.selectedCheckBoxes.contains(.withoutTags)
+                    },
+                    set: { isChecked in
+                        viewModel.toggleCondition(.withoutTags)
+                    }
+                ),
+                style: SwiftUICheckBox.Style(
+                    font: .subtitle02,
+                    textColor: Color(.text02!),
+                    iconColor: Color(.text02!),
+                    disabledIconColor: Color(.brandSecondary!)
+                )
+            )
+            SwiftUICheckBox(
+                title: "Very good",
+                text: "A lightly used item that may have slight imperfections.",
+                checked: Binding(
+                    get: {
+                        viewModel.selectedCheckBoxes.contains(.veryGood)
+                    },
+                    set: { isChecked in
+                        viewModel.toggleCondition(.veryGood)
+                    }
+                ),
+                style: SwiftUICheckBox.Style(
+                    font: .subtitle02,
+                    textColor: Color(.text02!),
+                    iconColor: Color(.text02!),
+                    disabledIconColor: Color(.brandSecondary!)
+                )
+            )
+            SwiftUICheckBox(
+                title: "Satisfactory",
+                text: "A frequently used item with imperfections and signs of wear.",
+                checked: Binding(
+                    get: {
+                        viewModel.selectedCheckBoxes.contains(.satisfactory)
+                    },
+                    set: { isChecked in
+                        viewModel.toggleCondition(.satisfactory)
+                    }
+                ),
+                style: SwiftUICheckBox.Style(
+                    font: .subtitle02,
+                    textColor: Color(.text02!),
+                    iconColor: Color(.text02!),
+                    disabledIconColor: Color(.brandSecondary!)
+                )
+            )
         }
-        .padding(.horizontal, 20)
     }
 }
